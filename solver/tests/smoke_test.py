06658fd1777126f07d4a67efff294db8cf66f59c
Jaro <jarohabiger@googlemail.com>
2017-11-25 15:35:46 +0100

    2 tests

--- solver/tests/smoke_test.py
+++ solver/tests/smoke_test.py
@@ -1,9 +1,31 @@
-def test_fail_on_invalid_input():
-    raise NotImplementedError
+from subprocess import Popen, PIPE
+from shlex import split
 
 
-def test_schema_conform_output():
-    raise NotImplementedError
+def test_fail_on_invalid_schema():
+    with open("tests/json/schema_fault.json") as f:
+        invalid_schema = f.read()
+
+    print(invalid_schema)
+
+    p = Popen(split("./solver"), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate(invalid_schema.encode("UTF-8"))
+
+    assert stdout.decode("UTF-8") == ""
+    assert "Traceback" in stderr.decode("UTF-8")
+
+
+def test_fail_on_invalid_semantic():
+    with open("tests/json/semantic_fault.json") as f:
+        invalid_schema = f.read()
+
+    print(invalid_schema)
+
+    p = Popen(split("./solver"), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate(invalid_schema.encode("UTF-8"))
+
+    assert stdout.decode("UTF-8") == ""
+    assert "Traceback" in stderr.decode("UTF-8")
 
 
 def test_valid_output():

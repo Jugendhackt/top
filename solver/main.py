1d8cfc760e4cb895bb601904ebcba19d4b0dcea2
lars <dessau.lars@icloud.com>
2017-11-25 14:47:32 +0100

    added checking against missing rooms

--- solver/main.py
+++ solver/main.py
@@ -7,10 +7,17 @@ def validate_contents(json_data):
     subject_ids = {s["id"] for s in json_data["subjects"]}
     student_subject_ids = {subject for student in json_data["students"] for subject in student["subjects"]}
     teacher_subject_ids = {subject for teacher in json_data["teachers"] for subject in teacher["subjects"]}
-    if len((student_subject_ids | teacher_subject_ids) - subject_ids) == 0:
-        print("yay")
-    else:
-        print("nay")
+    if len((student_subject_ids | teacher_subject_ids) - subject_ids) != 0:
+        raise ValueError("not all needed subjects exist!")
+
+    room_ids = {r["id"] for r in json_data["roomTypes"]}
+    subject_room_ids = {r["roomType"] for r in json_data["subjects"]}
+    if len(subject_room_ids - room_ids) != 0:
+        raise ValueError("not all needed rooms exist!")
+
+    room_count = {r["count"] for r in json_data["roomTypes"]}
+    if 0 in room_count:
+        raise ValueError("there is a room which exists zero times!")
 
 if __name__ == "__main__":
     with open('input_schema.json') as f:

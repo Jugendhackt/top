46ed10eb4c3116fbc6060d138df60891e6be8898
Jaro <jarohabiger@googlemail.com>
2017-11-26 03:44:06 +0100

    just throwing it up

---
+++ solver/src/generate_data.py
@@ -0,0 +1,53 @@
+from random import randint, choice, choices
+from json import dumps
+
+
+def generate():
+    room_counts = [25, 7, 7, 2, 1, 1]
+    room_types = []
+    for i, cnt in enumerate(room_counts):
+        room_types.append({
+            "id": i,
+            "count": cnt
+        })
+    rooms = []
+    for i, cnt in enumerate(room_counts):
+        for j in range(cnt):
+            rooms.append({
+                "id": i,
+                "count": cnt
+            })
+
+    subject_types = []
+    for i in range(3 * 25):
+        subject_types.append({
+            "id": i,
+            "hoursPerWeek": randint(2, 6),
+            "roomType": choice(rooms)
+        })
+
+    students = []
+    for i in range(randint(100, 1000)):
+        students.append({
+            "id": i,
+            "subjects": [x["id"] for x in choices(subject_types, k=10)]
+        })
+
+    teachers = []
+    for i in range(randint(50, 200)):
+        teachers.append({
+            "id": i,
+            "subjects": [x["id"] for x in choices(subject_types, k=2 * 3)],
+            "hoursPerWeek": randint(25, 40),
+            "blockedHours": [{
+                "dayOfWeek": randint(0, 5),
+                "hour": randint(0, 10)
+            } for _ in range(3)]
+        })
+
+    return {
+        "teachers": teachers,
+        "students": students,
+        "subjects": subject_types,
+        "roomTypes": room_types
+    }
--- solver/src/generator.py
+++ solver/src/generator.py
@@ -3,6 +3,7 @@ import numpy
 import json
 from numpy import int32
 
+import generate_data
 
 def parse_teachers(json_data):
     teacher_list = []
@@ -52,8 +53,38 @@ kurs = [raumbedarf, id]
 
 
 def generate(constraints):
-    pass
+    # construct the static env
+    hours_of_teachers = []
+    subjects_of_teachers = []
+    for teacher in constraints["teachers"]:
+        hours_of_teachers.append(teacher["hoursPerWeek"])
+        subjects_of_teachers.append(teacher["subjects"])# has a list of subjects for the teacher id = index
+
+    subjects_of_students = []
+    for student in constraints["students"]:
+        subjects_of_students.append(student["subjects"])
+
+    qty_of_room_type = []
+    for room_type in constraints["roomTypes"]:
+        qty_of_room_type.append(room_type["count"])
+
+    # construct a random solution
+    solution = []
+    all_the_subjects = []
+    for s in subjects_of_students:
+        all_the_subjects += s
+    subject_ranking = numpy.bincount(all_the_subjects)
+    subject_ranking = sorted(enumerate(subject_ranking), key=lambda x:x[1])
+    teacher_by_subjects = [] #list of teachers that can teach the subject with id = index
+    for subject in set(all_the_subjects):
+        teacher_by_subjects
+    for subject in subject_ranking:
+        pass
+    result = []
+    return result
 
 
 def mutate(probability=.1):
     pass
+
+generate(generate_data.generate())

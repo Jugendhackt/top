cda5df73baefda07b64741630b42a75db295c2c0
lars <dessau.lars@icloud.com>
2017-11-26 01:30:57 +0100

    fixed brackets

--- solver/tests/test_benchmark_hard.py
+++ solver/tests/test_benchmark_hard.py
@@ -5,7 +5,7 @@ from benchmark import benchmark
 
 correct_teachers = np.array([0])
 correct_students = np.array([[0]])
-correct_hour = np.full(3, -1)
+correct_hour = np.full((1,1,1), -1)
 correct_subjects = np.array([0])
 correct_used_room_type = np.array([0])
 correct_qty_of_room = np.array([1])
@@ -13,5 +13,6 @@ correct_qty_of_room = np.array([1])
 
 #sig: def benchmark(teachers, students, hours, subject, used_room_type, qty_of_room_type)
 def test_all_hards_valid():
+    assert len(correct_teachers) == len(correct_students) == len(correct_hour) == len(correct_subjects) == len(correct_used_room_type)
     test_var = benchmark(correct_teachers, correct_students, correct_hour, correct_subjects, correct_used_room_type, correct_qty_of_room)
-    assert test_var < 100, "fuck"
+    assert test_var < 100
